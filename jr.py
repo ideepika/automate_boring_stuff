3136d80c7d5fd2ff325a7546cdb55fbdd2deb22f
Deepika <deepikaupadhyay01@gmail.com>
2017-08-16 21:37:15 +0530

    enter text to roll no field submit_result
    to do select sem 
    solve captcha

--- jr.py
+++ jr.py
@@ -8,7 +8,18 @@ from selenium import webdriver
 driver = webdriver.Chrome('/home/deepika/chromedriver')
 
 
-# go to the jec results
+#to go to jec result page
 driver.get("http://www.jecjabalpur.ac.in/Exam/Programselect.aspx")
-#sbox = driver.find_element_by_class_name("txtSearch")
+sbox = driver.find_element_by_xpath("//input[@id='radlstProgram_0']").click()
 #sbox.send_keys("Roll No.")
+#to select the roll no field
+python_button = driver.find_element_by_xpath("//input[@id='txtrollno']").click()
+
+#type text
+text_area = driver.find_element_by_id('txtrollno')
+text_area.send_keys('0201IT151020')
+
+#click submit button
+submit_button = driver.find_element_by_xpath("//input[@id='btnviewresult']").click()
+# go to the jec results
+
