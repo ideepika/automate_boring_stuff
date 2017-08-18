04bef0c83689a78d7cb441c56a01fdc0a914c028
KrishnanandRai <knraikaip@gmail.com>
2017-08-18 15:27:50 +0530

    PEP 8 compliant code

--- jr.py
+++ jr.py
@@ -5,16 +5,16 @@ import time
 from selenium.common.exceptions import NoAlertPresentException
 
 
-
 def get_roll():
     global current_roll
     return_value = current_roll
-    current_roll = current_roll[ :-2] + str(int(current_roll[-2:]) + 1).zfill(2)
+    current_roll = current_roll[:-2] + str(int(current_roll[-2:]) + 1).zfill(2)
     return return_value
 
 
 def enter_captcha():
-    captcha = get_captcha_string("//*[@id='pnlCaptcha']/table/tbody/tr[1]/td/div/img", driver)
+    captcha = get_captcha_string(
+            "//*[@id='pnlCaptcha']/table/tbody/tr[1]/td/div/img", driver)
     # entering captcha
     driver.find_element_by_id('TextBox1').send_keys(captcha.upper())
 
@@ -30,13 +30,12 @@ def enter_captcha():
             alrt.accept()
             driver.find_element_by_xpath('//*[@id="TextBox1"]').clear()
             enter_captcha()
-        else :
+        else:
             alrt.accept()
             driver.find_element_by_xpath("//*[@id='btnReset']").click()
-            view_result(get_roll() , semester)
-    except NoAlertPresentException as e: 
+            view_result(get_roll(), semester)
+    except NoAlertPresentException as e:
         print("no alert")
-
 
 
 def view_result(roll_number, sem):
@@ -47,26 +46,28 @@ def view_result(roll_number, sem):
         text_area.send_keys(str(roll_number))
 
         # select semester
-        driver.find_element_by_xpath("//select[@id='drpSemester']/option[@value='"+str(sem)+"']").click()
+        driver.find_element_by_xpath(
+                "//select[@id='drpSemester']/option[@value='"+str(sem)+"']"
+                ).click()
 
         enter_captcha()
-        
+
         # deepika's function will be here
         print(roll_number)
         name = driver.find_element_by_xpath("//*[@id='lblNameGrading']")
         print(name.text)
         marks = driver.find_element_by_xpath("//*[@id='lblSGPA']")
         print(marks.text)
-        pass_status = driver.find_element_by_xpath("//*[@id='lblResultNewGrading']")
+        pass_status = driver.find_element_by_xpath(
+                "//*[@id='lblResultNewGrading']")
         print(pass_status.text)
-  
-
 
         driver.find_element_by_xpath("//*[@id='btnReset']").click()
-        view_result(get_roll() , semester)
+        view_result(get_roll(), semester)
     except:
         driver.quit()
-        driver = webdriver.Chrome('/home/sandesh/projects/automate_boring_stuff/chromedriver')
+        driver = webdriver.Chrome(
+                '/home/sandesh/projects/automate_boring_stuff/chromedriver')
         start_chrome()
         view_result(roll_number, sem)
 
@@ -97,10 +98,12 @@ def get_captcha_string(xpath, driver):
     with open(r"captcha.jpg", 'wb') as f:
         f.write(base64.b64decode(img_captcha_base64))
 
-    # cracking captha with tesseract (installed on ubuntu 16.04) and saving to text.text
-    system("tesseract -l eng /home/sandesh/projects/automate_boring_stuff/captcha.jpg text")
+    # cracking captha with tesseract (installed on ubuntu 16.04) and
+    # saving to text.text
+    system("tesseract -l eng" +
+           " /home/sandesh/projects/automate_boring_stuff/captcha.jpg text")
 
-    # reading captcha from text.txt 
+    # reading captcha from text.txt
     with open("text.txt") as captcha:
         captcha = captcha.readline().strip()
     return captcha
@@ -109,13 +112,15 @@ def get_captcha_string(xpath, driver):
 def start_chrome():
     # to go to jec result page
     driver.get("http://www.jecjabalpur.ac.in/Exam/Programselect.aspx")
-    sbox = driver.find_element_by_xpath("//input[@id='radlstProgram_0']").click()
+    sbox = driver.find_element_by_xpath(
+            "//input[@id='radlstProgram_0']").click()
 
 
 current_roll = input("Enter first roll no. ")
 semester = int(input("enter semester in integer: "))
 # Create a new instance of the chrome driver
-driver = webdriver.Chrome('/home/sandesh/projects/automate_boring_stuff/chromedriver')
+driver = webdriver.Chrome(
+        '/home/sandesh/projects/automate_boring_stuff/chromedriver')
 start_chrome()
 view_result(get_roll(), semester)
 # driver.execute_script("window.history.go(-1)")
