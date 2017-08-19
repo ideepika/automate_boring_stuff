ef0972bad77ec0f8c2f85e185674bed2ac43b6cd
KrishnanandRai <knraikaip@gmail.com>
2017-08-19 10:25:47 +0530

    added deepika's code to write results to csv file

--- jr.py
+++ jr.py
@@ -3,13 +3,35 @@ import base64
 from os import system
 import time
 from selenium.common.exceptions import NoAlertPresentException
+import csv
 
 
 def get_roll():
     global current_roll
     return_value = current_roll
     current_roll = current_roll[:-2] + str(int(current_roll[-2:]) + 1).zfill(2)
+    if len(current_roll) > len(return_value):
+        current_roll = input("enter first diploma roll: ").upper()
     return return_value
+
+def data_to_csv():
+    global driver
+    # return's output to unique id field
+    def get_elements_by_xpath(driver, xpath):
+        return [entry.text for entry in driver.find_elements_by_id(xpath)]
+
+    search_entries = [("Name", "lblNameGrading")
+        , ("RollNo", 'lblRollNoGrading')
+        , ("resultText", 'lblSGPA')
+        , ("pass_status", 'lblResultNewGrading')]
+    with open(out_file_name(), 'a') as f_output:
+        csv_output = csv.writer(f_output)
+        entries = []
+
+        # make entries in testfile.csv
+        for name, xpath in search_entries:
+            entries.append(get_elements_by_xpath(driver, xpath))
+        csv_output.writerows(zip(*entries))
 
 
 def enter_captcha():
@@ -52,7 +74,8 @@ def view_result(roll_number, sem):
 
         enter_captcha()
 
-        # deepika's function will be here
+        data_to_csv()
+
         print(roll_number)
         name = driver.find_element_by_xpath("//*[@id='lblNameGrading']")
         print(name.text)
@@ -64,7 +87,8 @@ def view_result(roll_number, sem):
 
         driver.find_element_by_xpath("//*[@id='btnReset']").click()
         view_result(get_roll(), semester)
-    except:
+    except Exception as e:
+        print(e)
         driver.quit()
         driver = webdriver.Chrome(
                 '/home/sandesh/projects/automate_boring_stuff/chromedriver')
@@ -112,15 +136,23 @@ def get_captcha_string(xpath, driver):
 def start_chrome():
     # to go to jec result page
     driver.get("http://www.jecjabalpur.ac.in/Exam/Programselect.aspx")
-    sbox = driver.find_element_by_xpath(
+    driver.find_element_by_xpath(
             "//input[@id='radlstProgram_0']").click()
 
 
-current_roll = input("Enter first roll no. ")
+def out_file_name():
+    return current_roll[4:6] + str(semester)+".csv"
+
+
+current_roll = input("Enter first roll no. ").upper()
 semester = int(input("enter semester in integer: "))
 # Create a new instance of the chrome driver
 driver = webdriver.Chrome(
         '/home/sandesh/projects/automate_boring_stuff/chromedriver')
 start_chrome()
+with open(out_file_name(), 'w') as f_output:
+    csv_output = csv.writer(f_output)
+    # Write header
+    csv_output.writerow(["Name", "RollNo", "resultText", "pass_status"])
 view_result(get_roll(), semester)
 # driver.execute_script("window.history.go(-1)")
